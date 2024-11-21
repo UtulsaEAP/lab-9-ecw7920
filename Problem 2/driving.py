96ef15bc7d6ba39eeb445c28caa870605e237f53
Your Name <ecw7920@utulsa.edu>
2024-11-21 11:52:15 -0600

    Lab 9

--- Problem 2/driving.py
+++ Problem 2/driving.py
@@ -1,10 +1,18 @@
 def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
    #write your code here
-   return
+   cost = (miles_driven / miles_per_gallon) * dollars_per_gallon
+   
+   return cost
 
 if __name__ == '__main__':
-    miles_per_gallon = float(input())
-    dollars_per_gallon = float(input())
     #print your costs here like example below
     #print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
-   
+    miles_per_gallon = float(input())
+    dollars_per_gallon = float(input())
+    
+    
+    for miles in [10.0, 50.0, 400.0]:
+           cost = driving_cost(miles_per_gallon, dollars_per_gallon, miles)
+           print(f'{cost:.2f}')
+    
+    
--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -1,10 +1,14 @@
 def feet_to_steps(user_feet):
    #write your code here
-   return
+   steps = user_feet / 2.5
+   return int(steps)
 
 if __name__ == '__main__':
     #take input feet steps here
+    user_feet = float(input())
     #store it into the function
+    steps = feet_to_steps(user_feet)
     
     #print your steps here
+    print(steps)
     feet_to_steps(5280)
