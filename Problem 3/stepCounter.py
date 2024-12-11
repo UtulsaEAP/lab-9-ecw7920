1ad2251338b394d6e35cb4d7ba883d892da67967
Elmer Wilson <ecw7920@utulsa.edu>
2024-12-11 14:29:37 -0600

    Lab 9

--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -4,7 +4,9 @@ def feet_to_steps(user_feet):
 
 if __name__ == '__main__':
     #take input feet steps here
+    user_feet = float(input())
     #store it into the function
-    
+    steps = user_feet / 2.5
     #print your steps here
+    print(int(steps))
     feet_to_steps(5280)
--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,14 +1,16 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
    
-   return user_val1, user_val2, user_val3, user_val4
+   return user_val2, user_val1, user_val4, user_val3
 
 if __name__ == '__main__':   
-   user_input1 = int(input())
-   user_input2 = int(input())
-   user_input3 = int(input())
-   user_input4 = int(input())
+   user_val1 = int(input())
+   user_val2 = int(input())
+   user_val3 = int(input())
+   user_val4 = int(input())
    #store output for every input here
    #print those output
+   swapped_values = swap_values(user_val1, user_val2, user_val3, user_val4)
+   print(swapped_values)
 
  
